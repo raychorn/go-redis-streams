e4dc50f73488c57d1f22ec6d629b62c483e9effa
Ray C Horn <raychorn@gmail.com>
2021-02-25 19:56:15 -0700

    updates.

--- producer/main.go
+++ producer/main.go
@@ -2,109 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
-	"encoding"
 
+	evt "github.com/felipeagger/go-redis-streams/packages/event"
 	"github.com/felipeagger/go-redis-streams/packages/utils"
 	"github.com/go-redis/redis/v7"
-	"github.com/vmihailenco/msgpack/v4"
 )
 
 var (
 	streamName string = os.Getenv("STREAM")
 	client     *redis.Client
 )
-
-////////////////////////////////////////////////////
-type UpdateEvent struct {
-	*Base
-	UserID  uint64
-	ConnectorId string
-	ConnectorStatus string
-}
-
-func (o *UpdateEvent) MarshalBinary() (data []byte, err error) {
-	return msgpack.Marshal(o)
-}
-
-func (o *UpdateEvent) UnmarshalBinary(data []byte) error {
-	return msgpack.Unmarshal(data, o)
-}
-
-type Type string
-
-const (
-	UpdateType Type = "Update"
-)
-
-type Base struct {
-	ID       string
-	ConnectorId string
-	ConnectorStatus string
-	Type     Type
-	DateTime time.Time
-	Retry    bool
-}
-
-// Event ...
-type Event interface {
-	GetID() string
-	GetConnectorId() string
-	GetConnectorStatus() string
-	GetType() Type
-	GetDateTime() time.Time
-	SetID(id string)
-	encoding.BinaryMarshaler
-	encoding.BinaryUnmarshaler
-}
-
-func New(t Type) (Event, error) {
-	b := &Base{
-		Type: t,
-	}
-
-	switch t {
-
-	case UpdateType:
-		return &UpdateEvent{
-			Base: b,
-		}, nil
-
-	}
-
-	return nil, fmt.Errorf("type %v not supported", t)
-}
-
-func (o *Base) GetID() string {
-	return o.ID
-}
-
-func (o *Base) SetID(id string) {
-	o.ID = id
-}
-
-func (o *Base) GetConnectorId() string {
-	return o.ConnectorId
-}
-
-func (o *Base) GetConnectorStatus() string {
-	return o.ConnectorStatus
-}
-
-func (o *Base) GetType() Type {
-	return o.Type
-}
-
-func (o *Base) GetDateTime() time.Time {
-	return o.DateTime
-}
-
-func (o *Base) String() string {
-
-	return fmt.Sprintf("id:%s type:%s", o.ID, o.Type)
-}
-////////////////////////////////////////////////////
 
 func init() {
 	var err error
@@ -116,29 +26,36 @@ func init() {
 
 func main() {
 	for {
-		generateEvent()
-		time.Sleep(10 * time.Second)
+		status := []string{"Active", "Not Active"}[rand.Intn(2)]
+		updateStatus(fmt.Sprintf("%d", rand.Intn(100)), status)
+		time.Sleep( 10 * time.Second)
 	}
 }
 
-func generateEvent() {
+func updateStatus(connectorID string, connectionsStatus string) {
+	comment := fmt.Sprintf("ConnectorId=%s, ConnectorStatus=%s", connectorID, connectionsStatus)
+	generateEvent(comment)
+}
+
+func generateEvent(comment string) {
 	var userID uint64 = 0
 	for i := 0; i < 10; i++ {
 
-		userID++
+		eventType := evt.CommentType
 
 		newID, err := produceMsg(map[string]interface{}{
-			"type": string(UpdateType),
-			"data": &UpdateEvent{
-				Base: &Base{
-					Type:     UpdateType,
+			"type": string(eventType),
+			"data": &evt.CommentEvent{
+				Base: &evt.Base{
+					Type:     eventType,
 					DateTime: time.Now(),
 				},
-				UserID: userID,
+				UserID:  userID,
+				Comment: comment,
 			},
 		})
 
-		checkError(err, newID, string(UpdateType), userID)
+		checkError(err, newID, string(eventType), userID, comment)
 
 	}
 }
